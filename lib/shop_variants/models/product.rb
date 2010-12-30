6e34321e4bb1ac58b77f59e0709de74618ac610c
Dirk Kelly <dk@dirkkelly.com>
2010-12-30 15:51:28 +0800

    0.1.1 gemspec

--- lib/shop_variants/models/product.rb
+++ lib/shop_variants/models/product.rb
@@ -4,7 +4,7 @@ module ShopVariants
       
       def self.included(base)
         base.class_eval do
-          has_many :variants, :class_name => 'ShopProductVariant', :foreign_key  => :product_id, :dependent => :destroy
+          has_many :variants, :class_name => 'ShopProductVariant', :foreign_key  => :product_id, :dependent => :destroy, :order => 'name ASC'
           
           accepts_nested_attributes_for :variants
         end
